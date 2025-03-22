1297d1af242c593f311c0e20a15be7f3be417c19
mydelis <fidelisokojie@gmail.com>
2025-03-22 18:45:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,6 @@ app.get('/fetch-price', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on 0.0.0.0:${PORT}`);
 });
